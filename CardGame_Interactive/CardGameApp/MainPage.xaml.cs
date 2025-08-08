96edce74e4bbb5d1853d551c2302aee57288b565
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-08-08 10:30:03 -0400

    Transform the composition relationship into aggregation

--- CardGame_Interactive/CardGameApp/MainPage.xaml.cs
+++ CardGame_Interactive/CardGameApp/MainPage.xaml.cs
@@ -5,12 +5,12 @@ public partial class MainPage : ContentPage
 {
     private CardGame _cardGame; //HAS-A relationship
 
-    public MainPage()
+    public MainPage(CardGame cardGame)
     {
         InitializeComponent();
 
         //create the card game
-        _cardGame = new CardGame(); //COMPOSITION RELATIONSHIP
+        _cardGame = cardGame; //AGGREGATION RELATIONSHIP
     }
 
 	private void OnDealCards(object sender, EventArgs e)
